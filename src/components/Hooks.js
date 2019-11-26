3f73c8900067f6a035030aeb5c05d549fce6207b
Rickard Åberg <rickard.aaberg@gmail.com>
2019-11-26 11:47:52 +0100

    added components

---
+++ src/components/Hooks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Hooks extends Component {
+  render() {
+    return (
+      <div>
+        Hooks
+      </div>
+    )
+  }
+}
+
+export default Hooks
---
+++ src/components/MyWorks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MyWorks extends Component {
+  render() {
+    return (
+      <div>
+        MyWorks
+      </div>
+    )
+  }
+}
+
+export default MyWorks
